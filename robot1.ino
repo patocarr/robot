5ec074f27262a59d9c8a4e62ee9fc32c7a6c076b
Pat <pato@patocarr.com>
2015-10-03 22:16:28 -0700

    Add 16x2 LCD with RGB backlight

--- robot1.ino
+++ robot1.ino
@@ -5,6 +5,8 @@
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_PWMServoDriver.h"
 #include <PID_v1.h>
+#include <LiquidCrystal.h>
+#include <Wire.h>
 
 #define trigPin 31
 #define echoPin 30
@@ -17,6 +19,10 @@
 #define IR6 27
 
 #define LED 13
+
+#define LCD_RED 3
+#define LCD_GREEN 5
+#define LCD_BLUE 6
 
 #define Motor1 1
 #define Motor2 2
@@ -135,6 +141,29 @@ double fSetpoint, fInput, fOutput;
 double fKp=3, fKi=5, fKd=2;
 PID followPID(&fInput, &fOutput, &fSetpoint, fKp, fKi, fKd, DIRECT);
 
+// LCD
+LiquidCrystal lcd(8, 9, 10, 11, 12, 13);
+
+int LCD_brightness = 255;
+
+void LCD_setBacklight(uint8_t r, uint8_t g, uint8_t b) {
+  // normalize the red LED - its brighter than the rest!
+  r = map(r, 0, 255, 0, 100);
+  g = map(g, 0, 255, 0, 150);
+ 
+  r = map(r, 0, 255, 0, LCD_brightness);
+  g = map(g, 0, 255, 0, LCD_brightness);
+  b = map(b, 0, 255, 0, LCD_brightness);
+ 
+  // common anode so invert!
+  r = map(r, 0, 255, 255, 0);
+  g = map(g, 0, 255, 255, 0);
+  b = map(b, 0, 255, 255, 0);
+  analogWrite(LCD_RED, r);
+  analogWrite(LCD_GREEN, g);
+  analogWrite(LCD_BLUE, b);
+}
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   Serial.begin(9600);
@@ -157,6 +186,14 @@ void setup() {
   pinMode(IR4, INPUT);
   pinMode(IR5, INPUT);
   pinMode(IR6, INPUT);
+
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  pinMode(LCD_RED, OUTPUT);
+  pinMode(LCD_GREEN, OUTPUT);
+  pinMode(LCD_BLUE, OUTPUT);
+  LCD_brightness = 100;
+  LCD_setBacklight(162, 40, 255); // light purple
 
   dSetpoint = 50;
   distPID.SetMode(AUTOMATIC);
@@ -205,6 +242,14 @@ void loop() {
   Serial.print(dOutput);
   Serial.print(" fOutput=");
   Serial.print(fOutput);
+
+  lcd.setCursor(0,0);
+  lcd.print("dOutput=");
+  lcd.print(dOutput);
+  lcd.setCursor(0,1);
+  lcd.print("fOutput=");
+  lcd.print(fOutput);
+
 
 //  switch (state) {
 //    case IDLE:
